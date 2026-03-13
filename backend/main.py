8832871ce9b80f8dd53a700c96d6338d42d2da8f
weblinkcon-dev <adebayowahab7@gmail.com>
2026-03-13 13:45:18 +0100

    fix: update CORS origins with all allowed domains

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,10 @@ app = FastAPI(title=settings.APP_NAME)
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173",
+                   "http://localhost:4173",
+                   "https://dph-website-qeq8.vercel.app/",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
